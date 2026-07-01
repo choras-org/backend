d946e68fec7abe761ff6cc99148e353e658a84c3
Ajat Darojat <ajatdarojat45@gmail.com>
2026-07-02 00:57:25 +0700

    feat: update Material schemas to use categoryId instead of category

--- app/schemas/material_schema.py
+++ app/schemas/material_schema.py
@@ -3,18 +3,19 @@ from marshmallow import Schema, fields
 
 class MaterialCreateSchema(Schema):
     name = fields.String(required=True)
-    category = fields.String(required=True)
+    categoryId = fields.Integer(required=True)
     description = fields.String(allow_none=True)
     absorptionCoefficients = fields.List(fields.Float())
 
 class MaterialUpdateSchema(Schema):
     name = fields.String(required=True)
-    category = fields.String(required=True)
+    categoryId = fields.Integer(required=True)
     description = fields.String(allow_none=True)
     absorptionCoefficients = fields.List(fields.Float())
 
 class MaterialSchema(MaterialCreateSchema):
     id = fields.Number()
     origin = fields.String()
+    category = fields.String(data_key="category", attribute="materialCategory.name")
     createdAt = fields.String()
     updatedAt = fields.String()
